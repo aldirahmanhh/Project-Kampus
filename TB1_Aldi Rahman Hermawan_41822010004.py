d9ca563c80227ebeee6e169577a01118cf10cebb
Aldi Rahman <94508058+aldirahmanhh@users.noreply.github.com>
2024-05-15 19:25:33 +0700

    Update TB1_Aldi Rahman Hermawan_41822010004.py

--- TB1_Aldi Rahman Hermawan_41822010004.py
+++ TB1_Aldi Rahman Hermawan_41822010004.py
@@ -1,13 +1,13 @@
 import re
 
-ukuran_input_pertama = input().rstrip().split()
-n = int(ukuran_input_pertama[0])
-m = int(ukuran_input_pertama[1])
+filename = "inputan.txt" # Nama File 
 
-matriks = []
-for _ in range(n):
-    item_matriks = input()
-    matriks.append(item_matriks)
+with open(filename, "r") as f:
+    ukuran_input_pertama = f.readline().rstrip().split()
+    n = int(ukuran_input_pertama[0])
+    m = int(ukuran_input_pertama[1])
+
+    matriks = [f.readline().rstrip() for _ in range(n)]
 
 hasil_dekode = ""
 for i in range(m):
@@ -16,11 +16,18 @@ for i in range(m):
             hasil_dekode += matriks[j][i]
         except IndexError:
             pass
-#Aldi Rahman Hermawan
-#41822010004
+# Aldi Rahman Hermawan
+# 41822010004
 pola = r'(?<=[\w])[^\w]+(?=[\w])'
 cocokan = re.findall(pola, hasil_dekode)
 
 for x in cocokan:
     hasil_dekode = hasil_dekode.replace(x, ' ', 1)
-print(hasil_dekode)
+
+hasil_file = open("hasil.txt", "w")
+hasil_file.write(hasil_dekode)
+hasil_file.close()
+
+print("File Hasil sudah dibuat silahkan dicek!")
+print("Nama File: hasil.txt")
+print("Hasil:", hasil_dekode)
